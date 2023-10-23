f155c94fb786a8a8f0e003c9eac6e6836ebf948b
Игорь <142589093+IgorPiro@users.noreply.github.com>
2023-10-23 15:26:01 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,36 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+    public static void main(String[] args) throws SQLException {
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("Иэн", "Кёртис", (byte) 23));
+        users.add(new User("Алексей", "Перминов", (byte) 24));
+        users.add(new User("Густав", "Ар", (byte) 21));
+        users.add(new User("Джеймс", "МакКормик", (byte) 20));
+        for (User us : users) {
+            userService.saveUser(us.getName(), us.getLastName(), (byte) us.getAge());
+            System.out.println("User с именем – " + us.getName() + " добавлен в базу данных");
+        }
+        List<User> usersTable = userService.getAllUsers();
+        for (User us : usersTable) {
+            System.out.println(us);
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,31 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USER = "root";
+    private static final String PASSWORD = "rootroot";
+
+    public Connection getConnection() {
+        Connection connection = null;
+
+        //System.out.println("Registering JDBC driver...");
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(Util.URL, Util.USER, Util.PASSWORD);
+            System.out.println("Соединение установлено");
+        } catch (SQLException th) {
+            System.out.println("Ошибка соединения!");
+            th.printStackTrace();
+        }
+
+        return connection;
+    }
 }
